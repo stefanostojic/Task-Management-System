994664851b27ee93066fdda10485ecd09721fa7e
Stefan Ostojic <stefanostojic98@gmail.com>
2021-03-26 14:23:56 +0100

    Added DbInitializer.

---
+++ Task Management App/Data/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Task_Management_System.Models;
+using System;
+using System.Linq;
+using Task_Management_System.Data;
+
+namespace Task_Management_System.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TaskManagementSystemContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var userRoles = new UserRole[]
+            {
+                new UserRole{ID=Guid.Parse("c8ae737c-ed9b-4fee-be6f-9c74ba376ae7"), Name="Administrator"},
+                new UserRole{ID=Guid.Parse("56860de3-f338-449c-be26-c946d4cb73b0"), Name="NormalUser"},
+                new UserRole{ID=Guid.Parse("5a1808ce-43b3-4e8b-9789-271b33b04f43"), Name="ProUser"}
+            };
+            foreach (UserRole ur in userRoles)
+            {
+                context.UserRoles.Add(ur);
+            }
+            context.SaveChanges();
+
+            var users = new User[]
+            {
+                new User{ID=Guid.Parse("c68af42e-fd7c-4ebb-a8d1-f714a47f60a7"), FirstName="Stefan", LastName="Ostojic", Email="s.ostojic@email.com", Password="123", UserRoleID=Guid.Parse("56860de3-f338-449c-be26-c946d4cb73b0")},
+                new User{ID=Guid.Parse("fe556bd9-f1bb-4f6a-b2b1-245f8daf9c08"), FirstName="Dušan", LastName="Krstić", Email="d.krstic@email.com", Password="123", UserRoleID=Guid.Parse("56860de3-f338-449c-be26-c946d4cb73b0")},
+                new User{ID=Guid.Parse("008127f1-efcd-4a74-8b26-cef9d4854c0c"), FirstName="Dejan", LastName="Tosenberger", Email="tosenberger@email.com", Password="123", UserRoleID=Guid.Parse("c8ae737c-ed9b-4fee-be6f-9c74ba376ae7")}
+            };
+            foreach (User u in users)
+            {
+                context.Users.Add(u);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Task Management App/Program.cs
+++ Task Management App/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Task_Management_System.Data;
 
 namespace Task_Management_App
 {
@@ -13,7 +15,29 @@ namespace Task_Management_App
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TaskManagementSystemContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
