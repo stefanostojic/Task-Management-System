3966ed6385a676a25411bf83ccf568a1a5be286d
Stefan Ostojic <stefanostojic98@gmail.com>
2021-03-26 14:25:07 +0100

    Added DB context.

---
+++ Task Management App/Data/TaskManagementSystemContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Task_Management_System.Models;
+
+namespace Task_Management_System.Data
+{
+    public class TaskManagementSystemContext : DbContext
+    {
+        public TaskManagementSystemContext(DbContextOptions<TaskManagementSystemContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Label> Labels { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProPlan> ProPlans { get; set; }
+        public DbSet<ProUser> ProUsers { get; set; }
+        public DbSet<Subtask> Subtasks { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TaskGroup> TaskGroups { get; set; }
+        public DbSet<TaskRole> TaskRoles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserTask> UserTasks { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
--- Task Management App/Startup.cs
+++ Task Management App/Startup.cs
@@ -19,6 +19,10 @@ namespace Task_Management_App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TaskManagementSystemContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddControllersWithViews();
 
