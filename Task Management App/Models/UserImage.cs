56f31216cc87c169ef2608a4b3f63566a9cadf49
Stefan Ostojic <stefanostojic98@gmail.com>
2021-03-28 23:21:58 +0200

    Added UserImage model.

---
+++ Task Management App/Models/UserImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Management_System.Models
+{
+    public class UserImage : Image
+    {
+        public Guid UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
