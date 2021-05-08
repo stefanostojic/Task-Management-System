6f4338a712b628bea386fd076904a62bad3f4360
Stefan Ostojic <stefanostojic98@gmail.com>
2021-05-08 23:38:19 +0200

    Refactored DbInitializer.

--- Task Management App/Data/DbInitializer.cs
+++ Task Management App/Data/DbInitializer.cs
@@ -20,7 +20,7 @@ namespace Task_Management_System.Data
             context.Database.EnsureDeleted();
             context.Database.Migrate();
 
-
+            #region UserRoles
             var userRoles = new UserRole[]
             {
                 new UserRole {
@@ -41,7 +41,28 @@ namespace Task_Management_System.Data
                 context.UserRoles.Add(ur);
             }
             context.SaveChanges();
+            #endregion
 
+            #region Images
+            var images = new Image[]
+            {
+                new Image {
+                    ID = Guid.Parse("065e4bc3-b0ec-4a5f-9b0d-a9814daa977f"),
+                    FilePath = "Sample image file path 1"
+                },
+                new Image {
+                    ID = Guid.Parse("9374a199-a242-44d1-8593-4375985b0b17"),
+                    FilePath = "Sample image file path 2"
+                }
+            };
+            foreach (Image i in images)
+            {
+                context.Images.Add(i);
+            }
+            context.SaveChanges();
+            #endregion
+            
+            #region Users
             var users = new User[]
             {
                 new User {
@@ -51,7 +72,7 @@ namespace Task_Management_System.Data
                     Email = "s.ostojic@email.com",
                     Password = "123",
                     UserRoleID = Guid.Parse("56860de3-f338-449c-be26-c946d4cb73b0"),
-                    ImageID = null
+                    ImageID = Guid.Parse("065e4bc3-b0ec-4a5f-9b0d-a9814daa977f")
                 }, // NormalUser
                 new User {
                     ID = Guid.Parse("fe556bd9-f1bb-4f6a-b2b1-245f8daf9c08"),
@@ -60,7 +81,7 @@ namespace Task_Management_System.Data
                     Email = "d.krstic@email.com",
                     Password = "123",
                     UserRoleID = Guid.Parse("56860de3-f338-449c-be26-c946d4cb73b0"),
-                    ImageID = null
+                    ImageID = Guid.Parse("9374a199-a242-44d1-8593-4375985b0b17")
                 }, // NormalUser
                 new User {
                     ID = Guid.Parse("008127f1-efcd-4a74-8b26-cef9d4854c0c"),
@@ -77,7 +98,9 @@ namespace Task_Management_System.Data
                 context.Users.Add(u);
             }
             context.SaveChanges();
-
+            #endregion
+            
+            #region Projects
             var projects = new Project[]
             {
                 new Project {
@@ -116,7 +139,9 @@ namespace Task_Management_System.Data
                 context.Projects.Add(p);
             }
             context.SaveChanges();
+            #endregion
 
+            #region TaskGroups
             var taskGroups = new TaskGroup[]
             {
                 new TaskGroup {
@@ -137,7 +162,9 @@ namespace Task_Management_System.Data
                 context.TaskGroups.Add(tg);
             }
             context.SaveChanges();
+            #endregion
 
+            #region Tasks
             var tasks = new Task[]
             {
                 new Task {
@@ -146,7 +173,7 @@ namespace Task_Management_System.Data
                     Description = "Potrebno je dizajnirati novi sajt Erste banke.",
                     Finished = false,
                     DueDate = DateTime.Parse("2021-04-15T09:00:00"),
-                    //TaskGroupID = Guid.Parse("cfaba562-3760-4065-8a64-2020e03442de")
+                    TaskGroupID = Guid.Parse("cfaba562-3760-4065-8a64-2020e03442de")
                 }, // Stefan, Erste banka, Sajt
                 new Task {
                     ID = Guid.Parse("ff237276-30b2-42ef-ae5d-155e3255486f"),
@@ -154,7 +181,7 @@ namespace Task_Management_System.Data
                     Description = "Potrebno je isprojektovati bazu podataka za novi sajt Erste banke.",
                     Finished = false,
                     DueDate = DateTime.Parse("2021-04-25T10:00:00"),
-                    //TaskGroupID = Guid.Parse("cfaba562-3760-4065-8a64-2020e03442de")
+                    TaskGroupID = Guid.Parse("cfaba562-3760-4065-8a64-2020e03442de")
                 } // Stefan, Erste banka, Sajt
             };
             foreach (Task t in tasks)
@@ -162,20 +189,39 @@ namespace Task_Management_System.Data
                 context.Tasks.Add(t);
             }
             context.SaveChanges();
+            #endregion
 
+            #region TaskRoles
+            var taskRoles = new TaskRole[]
+            {
+                new TaskRole {
+                    ID = Guid.Parse("02bfe263-c45c-4e32-8c9f-fb3bc3f2be66"),
+                    Name = "Programer"
+                }, 
+                new TaskRole {
+                    ID = Guid.Parse("274e5f62-bddc-4aa7-aec2-ae0ee925ef5b"),
+                    Name = "Dizajner"
+                }
+            };
+            foreach (TaskRole tr in taskRoles)
+            {
+                context.TaskRoles.Add(tr);
+            }
+            context.SaveChanges();
+            #endregion
+
+            #region UserTasks
             var userTasks = new UserTask[]
             {
                 new UserTask {
-                    ID = Guid.Parse("2253ca8a-2300-4459-8d52-912196472f03"),
                     AssignmentDate = DateTime.Parse("2021-03-26T09:00:00"),
                     EstimatedEndDate = DateTime.Parse("2021-04-01T10:00:00"),
                     ActualEndDate = DateTime.Parse("2021-03-29T11:00:00"),
                     UserID = Guid.Parse("fe556bd9-f1bb-4f6a-b2b1-245f8daf9c08"),
                     TaskID = Guid.Parse("304ea812-5153-4992-a8bc-da8c22ab1fad"),
-                    TaskRole = null
-                }, // Stefan, Erste banka, Sajt, Dizajn
+                    TaskRoleID = Guid.Parse("274e5f62-bddc-4aa7-aec2-ae0ee925ef5b")
+                }, // Stefan, Erste banka, Sajt, Dizajn, Dizajner
                 new UserTask {
-                    ID = Guid.Parse("59b44150-133f-4d77-b0f7-e55855943910"),
                     AssignmentDate = DateTime.Parse("2021-03-26T09:00:00"),
                     EstimatedEndDate = DateTime.Parse("2021-04-01T10:00:00"),
                     ActualEndDate = DateTime.Parse("2021-03-29T11:00:00"),
@@ -189,6 +235,8 @@ namespace Task_Management_System.Data
                 context.UserTasks.Add(ut);
             }
             context.SaveChanges();
+            #endregion
+            
         }
     }
 }
