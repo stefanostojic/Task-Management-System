a9c927750489bb5d529454371676ebed61d97d4b
Stefan Ostojic <stefanostojic98@gmail.com>
2021-05-08 23:21:08 +0200

    Swagger additionally configured.

---
+++ Task Management App/Options/SwaggerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Management_System.Options
+{
+    public class SwaggerOptions
+    {
+    }
+}
--- Task Management App/Startup.cs
+++ Task Management App/Startup.cs
@@ -1,10 +1,13 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
 using Task_Management_System.Data;
 
 namespace Task_Management_System
@@ -38,6 +41,40 @@ namespace Task_Management_System
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddSwaggerGen();
+            //services.AddSwaggerGen(setupAction =>
+            //{
+            //    setupAction.SwaggerDoc("ExamRegistrationOpenApiSpecification",
+            //        new Microsoft.OpenApi.Models.OpenApiInfo()
+            //        {
+            //            Title = "Student Exam Registration API",
+            //            Version = "1",
+            //            //Često treba da dodamo neke dodatne informacije
+            //            Description = "Pomoću ovog API-ja može da se vrši prijava ispita, modifikacija prijava ispita kao i pregled kreiranih prijava ispita.",
+            //            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            //            {
+            //                Name = "Marko Marković",
+            //                Email = "marko@mail.com",
+            //                Url = new Uri("http://www.ftn.uns.ac.rs/")
+            //            },
+            //            License = new Microsoft.OpenApi.Models.OpenApiLicense
+            //            {
+            //                Name = "FTN licence",
+            //                Url = new Uri("http://www.ftn.uns.ac.rs/")
+            //            },
+            //            TermsOfService = new Uri("http://www.ftn.uns.ac.rs/examRegistrationTermsOfService")
+            //        });
+
+            //    //Pomocu refleksije dobijamo ime XML fajla sa komentarima (ovako smo ga nazvali u Project -> Properties)
+            //    var xmlComments = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
+
+            //    //Pravimo putanju do XML fajla sa komentarima
+            //    var xmlCommentsPath = Path.Combine(AppContext.BaseDirectory, xmlComments);
+
+            //    //Govorimo swagger-u gde se nalazi dati xml fajl sa komentarima
+            //    setupAction.IncludeXmlComments(xmlCommentsPath);
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +88,16 @@ namespace Task_Management_System
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
