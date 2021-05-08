76bc8515a9344c5b4d56826e7a5274cbb592b447
Stefan Ostojic <stefanostojic98@gmail.com>
2021-05-08 23:39:44 +0200

    Trying to implement a generic repository.

---
+++ Task Management App/Data/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Management_System.Data
+{
+    public interface IRepository<T> 
+    {
+        IQueryable<T> GetAll();
+
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task RemoveAsync(T entity);
+    }
+}
