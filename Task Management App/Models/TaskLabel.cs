84411aed321f1ee7a32f6108154c79f28b086ee9
Stefan Ostojic <stefanostojic98@gmail.com>
2021-05-08 23:26:53 +0200

    Added ProjectRole and TaskLabel models.

---
+++ Task Management App/Models/TaskLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Management_System.Models
+{
+    public class TaskLabel
+    {
+        public Guid TaskID { get; set; }
+        public virtual Task Task { get; set; }
+        public Guid LabelID { get; set; }
+        public virtual Label Label { get; set; }
+    }
+}
